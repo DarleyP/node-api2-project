9199ed5501e19cc597f47b7bf8c32c5ce72825b5
Darley Peralta <ordeal00s@gmail.com>
2023-08-18 19:38:37 -0400

    siding server routes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+
+const server = require('./api/server')
+
+const Port = 5000
+
+
+server.listen(Port, () => {
+    console.log('listening on ', Port)
+})
